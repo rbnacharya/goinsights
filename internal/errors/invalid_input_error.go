6583e474cdf7dc2ab75f9847471155162e624177
Rabin <rbnacharya@gmail.com>
2024-03-20 09:33:52 +0100

    reformatting

--- internal/errors/invalid_input_error.go
+++ internal/errors/invalid_input_error.go
@@ -26,21 +26,19 @@ func NewBoom(code string, message string, details interface{}) *Boom {
 type Booms struct {
 	Errors []Boom `json:"errors"`
 
-	HasBooms func() bool `json:"-"`
+	// HasBooms func() bool `json:"-"`
+}
+
+func (b *Booms) HasBooms() bool {
+	return len(b.Errors) > 0
 }
 
 func (b *Booms) AddBoom(boom *Boom) {
 	b.Errors = append(b.Errors, *boom)
-	b.HasBooms = func() bool {
-		return len(b.Errors) > 0
-	}
 }
 
 func NewBooms() *Booms {
 	booms := &Booms{}
-	booms.HasBooms = func() bool {
-		return len(booms.Errors) > 0
-	}
 	return booms
 }
 
